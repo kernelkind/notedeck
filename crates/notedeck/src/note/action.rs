8ff9a144366e4ee26ceceb409aa566912faf0b80
kernelkind <kernelkind@gmail.com>
2025-04-18 22:38:04 -0500

    add `MediaAction`
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck/src/note/action.rs
+++ crates/notedeck/src/note/action.rs
@@ -24,6 +24,9 @@ pub enum NoteAction {
 
     /// User has clicked the zap action
     Zap(ZapAction),
+
+    /// User clicked on media
+    Media(MediaAction),
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
@@ -36,4 +39,22 @@ pub enum ZapAction {
 pub struct ZapTargetAmount {
     pub target: NoteZapTargetOwned,
     pub specified_msats: Option<u64>, // if None use default amount
+}
+
+#[derive(Debug)]
+pub enum MediaAction {
+    Unblur(String), // URL to unblur
+}
+
+impl MediaAction {
+    pub fn process(&self, ui: &egui::Ui) {
+        match &self {
+            MediaAction::Unblur(url) => send_unblur_signal(ui.ctx(), url),
+        }
+    }
+}
+
+fn send_unblur_signal(ctx: &egui::Context, url: &str) {
+    let id = egui::Id::new(("blur", url));
+    ctx.data_mut(|d| d.insert_temp(id, false))
 }
