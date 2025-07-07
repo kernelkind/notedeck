2dda5806387bcf31b786c20997b7e97ff8cf51ef
kernelkind <kernelkind@gmail.com>
2025-07-07 17:53:13 -0400

    Unify sub for contacts in accounts & timeline
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck_columns/src/ui/add_column.rs
+++ crates/notedeck_columns/src/ui/add_column.rs
@@ -622,6 +622,7 @@ pub fn render_add_column_routes(
                     ctx.pool,
                     ctx.note_cache,
                     app.since_optimize,
+                    ctx.accounts,
                 );
 
                 app.columns_mut(ctx.accounts)
@@ -663,6 +664,7 @@ pub fn render_add_column_routes(
                             ctx.pool,
                             ctx.note_cache,
                             app.since_optimize,
+                            ctx.accounts,
                         );
 
                         app.columns_mut(ctx.accounts)
