ce1a7d54d656f74e9ab3b44b2809425dc5e178d7
kernelkind <kernelkind@gmail.com>
2025-09-01 13:38:36 -0400

    make endpoint error into struct
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck/src/error.rs
+++ crates/notedeck/src/error.rs
@@ -33,7 +33,7 @@ pub enum ZapError {
     #[error("invalid lud16")]
     InvalidLud16(String),
     #[error("invalid endpoint response")]
-    EndpointError(String),
+    EndpointError(EndpointError),
     #[error("bech encoding/decoding error")]
     Bech(String),
     #[error("serialization/deserialization problem")]
@@ -41,6 +41,15 @@ pub enum ZapError {
     #[error("nwc error")]
     NWC(String),
 }
+
+impl ZapError {
+    pub fn endpoint_error(error: String) -> ZapError {
+        ZapError::EndpointError(EndpointError(error))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct EndpointError(pub String);
 
 impl From<String> for Error {
     fn from(s: String) -> Self {
