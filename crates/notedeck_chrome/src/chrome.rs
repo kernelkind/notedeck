b45554b380d6b3958e564844510b74bd3c914b6e
kernelkind <kernelkind@gmail.com>
2025-11-20 15:04:09 -0700

    fix(nav-drawer): spacing issue
    when opening and closing the nav drawer, the spacing of the whole
    app would change. Introduced in
    db9005e4034268b0504c4bf133f82a5f16d920f3
    
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck_chrome/src/chrome.rs
+++ crates/notedeck_chrome/src/chrome.rs
@@ -317,6 +317,7 @@ impl Chrome {
             .vertical(|mut strip| {
                 // the actual content, shifted up because of the soft keyboard
                 strip.cell(|ui| {
+                    ui.spacing_mut().item_spacing = prev_spacing;
                     action = self.panel(ctx, ui, keyboard_height);
                 });
 
@@ -328,7 +329,6 @@ impl Chrome {
                     }
                 });
             });
-        ui.spacing_mut().item_spacing = prev_spacing;
 
         // hovering virtual keyboard
         if virtual_keyboard {
