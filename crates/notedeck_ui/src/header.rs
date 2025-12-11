c3e5b8a4361dcc8dbe7c3fb08ba508ea510138f2
kernelkind <kernelkind@gmail.com>
2025-12-11 14:52:53 -0600

    refactor(nav): move chevron to notedeck_ui
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck_ui/src/header.rs
+++ crates/notedeck_ui/src/header.rs
@@ -1,4 +1,4 @@
-use egui::{Sense, UiBuilder};
+use egui::{Sense, Stroke, UiBuilder};
 
 pub struct NavHeaderCore {}
 
@@ -26,4 +26,26 @@ impl NavHeaderCore {
             ui.advance_cursor_after_rect(rect);
         });
     }
+}
+
+pub fn chevron(
+    ui: &mut egui::Ui,
+    pad: f32,
+    size: egui::Vec2,
+    stroke: impl Into<Stroke>,
+) -> egui::Response {
+    let (r, painter) = ui.allocate_painter(size, egui::Sense::click());
+
+    let min = r.rect.min;
+    let max = r.rect.max;
+
+    let apex = egui::Pos2::new(min.x + pad, min.y + size.y / 2.0);
+    let top = egui::Pos2::new(max.x - pad, min.y + pad);
+    let bottom = egui::Pos2::new(max.x - pad, max.y - pad);
+
+    let stroke = stroke.into();
+    painter.line_segment([apex, top], stroke);
+    painter.line_segment([apex, bottom], stroke);
+
+    r
 }
